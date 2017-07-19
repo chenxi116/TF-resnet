ce63c6b90c420f6a1c1f037b274724f111f5c682
Chenxi Liu <chenxi_liu@live.cn>
2017-07-19 11:17:26 -0400

    add LICENSE

--- npy2tfmodel.py
+++ npy2tfmodel.py
@@ -1,12 +1,27 @@
+# Copyright 2017 Chenxi Liu. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+# sample usage:
+# python npy2tfmodel.py 0 ./model/ResNet101.npy ./model/ResNet101_init.tfmodel
+
 import numpy as np
 import tensorflow as tf
 import resnet_model
 import sys
 import os; os.environ['CUDA_VISIBLE_DEVICES'] = sys.argv[1]
 import pdb
-
-# sample usage:
-# python npy2tfmodel.py 0 ./model/ResNet101.npy ./model/ResNet101_init.tfmodel
 
 weights = np.load(sys.argv[2])[()]
 # fully connected -> fully convolutional
@@ -30,4 +45,3 @@ assert(count == len(weights))
 
 snapshot_saver = tf.train.Saver()
 snapshot_saver.save(sess, sys.argv[3])
-# pdb.set_trace()
